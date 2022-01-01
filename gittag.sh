d390e5a40bbe704a8cf1fb64aad7322b40c7f41b
HasinduLanka <hlrathnayaka@gmail.com>
2022-01-01 16:29:15 +0530

    Tags now begins with v for better golang pkg support

--- gittag.sh
+++ gittag.sh
@@ -2,7 +2,7 @@
 
 go mod tidy
 
-git tag $1 -m "Release $1"
-git push origin $1
+git tag v$1 -m "Release $1"
+git push origin v$1
 
-GOPROXY=proxy.golang.org go list -m github.com/spindlygo/spindly@$1
+GOPROXY=proxy.golang.org go list -m github.com/spindlygo/spindly@v$1
